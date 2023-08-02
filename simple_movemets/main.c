6ea5c898ac2c22ef2c8a5317edc78e31495e8f9e
Jolande Steenput <jsteenpu@mi-r2-p5.s19.be>
2023-08-02 13:26:17 +0200

    udpate simple movements code OK

--- simple_movemets/main.c
+++ simple_movemets/main.c
@@ -6,49 +6,24 @@
 /*   By: jsteenpu <jsteenpu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 12:03:02 by jsteenpu          #+#    #+#             */
-/*   Updated: 2023/08/01 13:28:25 by jsteenpu         ###   ########.fr       */
+/*   Updated: 2023/08/02 13:24:21 by jsteenpu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "simple.h"
+#include <stdio.h>
 
 
-int	handle_input(int keycode, void *param)
-{
-	t_program	*program_ptr;
-
-	// initializing the void t_program_ptr and typecasting 
-	program_ptr = (t_program *)param;
-
-	// if no event is hooked, close the window ?
-	mlx_clear_window(program_ptr->mlx_ptr, program_ptr->win_ptr);
-	
-	// move in the correct direction using the keycodes
-	// move in relation to the size of the image
-	if (keycode == 123) // LEFT KEY
-		program_ptr->x -= program_ptr->image.image_width_x;
-	if (keycode == 124) // RIGTH KEY
-		program_ptr->x += program_ptr->image.image_width_x;
-	if (keycode == 125) // DOWN KEY
-		program_ptr->y += program_ptr->image.image_heigth_y;
-	if (keycode == 126) // UP KEY
-		program_ptr->y -= program_ptr->image.image_heigth_y;
-	
-	// put the image to the window
-	mlx_put_image_to_window(program_ptr->mlx_ptr, program_ptr->win_ptr, program_ptr->image.image_ptr, program_ptr->image.image_width_x, program_ptr->image.image_heigth_y);
-	
-	printf("Keycode pressed: %d\n", keycode);
-	
-	return (0);
-}
+// int test(int key)
+// {
+// 	printf("key = %d\n", key);
+// 	return (0);
+// }
 
 int	main(void)
 {
-	t_image		*img;
-	char		*filename = "cursor.xpm";
+	char		*filename = "minion.xpm";
 	t_program	program;
-	int			width;
-	int			heigth;
 	
 	// initialize the connection
 	program.mlx_ptr = mlx_init();
@@ -56,16 +31,17 @@ int	main(void)
 	// open new window - window connector
 	program.win_ptr = mlx_new_window(program.mlx_ptr, 1920, 1080, "My first simple game");
 
-	// import image
-	// how to resize an image
-	img->img_ptr = mlx_xpm_file_to_image(program.mlx_ptr, filename, &img->image_width_x, &img->image_heigth_y);
-
-	// put image to window at x = 0 and y = 0
-	//mlx_put_image_to_window(program.mlx_ptr, program.win_ptr, img, 0, 0);
-
-	// hooks setup 
+	// create a new image -- return the initialized image
+	// this is the new sprite
+	program.sprite = ft_sprite(program.mlx_ptr, filename);
+	program.x = 0;
+	program.y = 0;
+	mlx_put_image_to_window(program.mlx_ptr, program.win_ptr, program.sprite.img_ptr, program.x, program.y);
+	
+	/* hooks setup */
+	//mlx_key_hook(program.win_ptr, &test, NULL);
 	mlx_key_hook(program.win_ptr, *handle_input, &program);
-
+	mlx_loop_hook(program.mlx_ptr, *ft_update, &program);
 	
 	// keep the window opem
 	mlx_loop(program.mlx_ptr);
--- simple_movemets/simple.h
+++ simple_movemets/simple.h
@@ -6,7 +6,7 @@
 /*   By: jsteenpu <jsteenpu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 12:00:18 by jsteenpu          #+#    #+#             */
-/*   Updated: 2023/08/01 13:07:21 by jsteenpu         ###   ########.fr       */
+/*   Updated: 2023/08/02 13:23:47 by jsteenpu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,48 @@
 # include "mlx.h"
 # include <stdio.h>
 
+
+/* -----------------------------------------------------------*/
+
+#ifndef ANIMATION_FRAMES
+# define ANIMATION_FRAMES 10
+#endif
+
+
+#define LEFT_KEY 123
+#define RIGHT_KEY 124
+#define UP_KEY	126
+#define	DOWN_KEY 125		
+
 /* ------------------STRUCTURES ---------------------------------*/
 
 // for the connection and new window 
 
-typedef struct s_program
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
-	t_image	image;
-	int		x;
-	int		y;
-}	t_program;
-
 typedef struct s_image 
 {
 	void	*img_ptr;
-	int		image_width_x;
-	int		image_heigth_y;
+	void	*address;
+	int		width_x;
+	int		heigth_y;
 	int		bits_per_pixel;
 	int		line_size;
 	int		endian;
 }	t_image;
 
+typedef struct s_program
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_image	sprite;
+	int		x;
+	int		y;
+}	t_program;
+
+/* ------------------ FUNCTIONS ---------------------------------*/
+
+t_image	ft_sprite(void *mlx_ptr, char *path);
+
+int	handle_input(int keycode, void *param);
+int	ft_update (void *param);
 
 #endif
