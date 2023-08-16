b2a7e2170c42c0375e37c63bed62509491a78b44
Jolande Steenput <jsteenpu@mi-r2-p5.s19.be>
2023-08-16 13:19:41 +0200

    terminate_game function created

--- game/controls.c
+++ game/controls.c
@@ -6,7 +6,7 @@
 /*   By: jsteenpu <jsteenpu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 09:38:36 by jolandestee       #+#    #+#             */
-/*   Updated: 2023/08/16 12:11:43 by jsteenpu         ###   ########.fr       */
+/*   Updated: 2023/08/16 13:18:05 by jsteenpu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,10 @@ int  key_controls(int keycode, t_map *game)
         game->start_x = move_right(game->map, game->start_y, game->start_x, game->token);
     else
 		printf("other key was pressed: %d\n", keycode);
-    if (game->start_x || game->start_y)
+    printf("the player's new position (%d, %d)\n", game->start_y, game->start_x);
+    if (game->start_x == 0 || game->start_y == 0)
         terminate_game();
+    printf("OK\n"); 
     adding_in_graphics(game);
     printf("the number of tokens gathered: %d\n", game->token);
 	return (0);
