1f5b42bca84298ac158e782e9ea0ed99943f832f
Jolande Steenput <jsteenpu@mi-r2-p5.s19.be>
2023-08-17 12:30:40 +0200

    playing around with visuals

--- game/graphics.c
+++ game/graphics.c
@@ -6,21 +6,21 @@
 /*   By: jsteenpu <jsteenpu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 12:48:47 by jsteenpu          #+#    #+#             */
-/*   Updated: 2023/08/17 11:40:15 by jsteenpu         ###   ########.fr       */
+/*   Updated: 2023/08/17 12:29:54 by jsteenpu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/so_long.h"
 
-void	set_player_map(t_map *game)
-{
-	int	x;
-	int	y;
+// void	set_player_map(t_map *game)
+// {
+// 	int	x;
+// 	int	y;
 	
-	game->player_img = mlx_xpm_file_to_image(game->mlx_ptr, "images/cat_010.xpm", &x, &y);
-	game->player_img->width = x;
-	game->player_img->heigth = y;
-}
+	
+// 	game->player_img->width = x;
+// 	game->player_img->heigth = y;
+// }
 
 
 void	init_game_images(t_map *game)
@@ -31,10 +31,10 @@ void	init_game_images(t_map *game)
 	// initialize the image -- return void pointer as the image initializer
 	// Creates one that contains the .xpm image found in relative_path
 	// and saves its width and height in pixels to the given pointers.
-	game->wall = mlx_xpm_file_to_image(game->mlx_ptr, "images/bark_01.xpm", &x, &y);
+	game->wall = mlx_xpm_file_to_image(game->mlx_ptr, "game_images/wall.xpm", &x, &y);
 	printf("the wall image width: %d\n", x);
 	printf("the wall image heigth: %d\n", y);
-	game->floor = mlx_xpm_file_to_image(game->mlx_ptr, "images/floor_grass_tile.xpm", &x, &y);
+	game->floor = mlx_xpm_file_to_image(game->mlx_ptr, "game_images/floor.xpm", &x, &y);
 	printf("the floor image width: %d\n", x);
 	printf("the floor image heigth: %d\n", y);
 	game->exit_img = mlx_xpm_file_to_image(game->mlx_ptr, "game_images/exit.xpm", &x, &y);
@@ -43,6 +43,9 @@ void	init_game_images(t_map *game)
 	game->collectible_img = mlx_xpm_file_to_image(game->mlx_ptr, "game_images/item.xpm", &x, &y);
 	printf("the collectibles image width: %d\n", x);
 	printf("the collectibles imgage heigth: %d\n", y);
+	game->player_img = mlx_xpm_file_to_image(game->mlx_ptr, "images/cat_010.xpm", &x, &y);
+	printf("the player image width: %d\n", x);
+	printf("the player imgage heigth: %d\n", y);
 }
 
 void	adding_in_graphics(t_map *game)
@@ -64,7 +67,7 @@ void	adding_in_graphics(t_map *game)
 				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->collectible_img, width * IMG_SIZE, height * IMG_SIZE);
 			if (game->map[height][width] == 'P') // player
 			{
-				set_player_map(game);
+				//set_player_map(game);
 				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->player_img, width * IMG_SIZE, height * IMG_SIZE);
 			}
 			if (game->map[height][width] == 'E') // exit
